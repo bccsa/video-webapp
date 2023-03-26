d70b02a14a16bb7e1e1c93b52a08e4f079307d0a
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2023-03-26 22:50:03 +0200

    live video player

--- client/controls/appFrame.js
+++ client/controls/appFrame.js
@@ -3,48 +3,44 @@ class appFrame extends ui {
         super();
 
         this.title = "Title comes here"
-        this.description = "Description comes here. This is a description. Read it carefully"
+        this.description = "Description comes here. Description comes here. Description comes here. Description comes here. Description comes here. Description comes here.";
+
     }
 
     get html() {
         return `
-        <!-- menu -->
-        <div class="fixed bottom-0 left-0 right-0 h-14 bg-slate-900">
+        <div class="flex flex-col h-screen bg-slate-800">
+            <!-- title -->
+            <div class="w-full pl-4 pr-4 h-12  bg-slate-900 flex ">
+                <span class="text-slate-400 font-sans text-lg flex items-center">@{title}</span>
+            </div>
 
-        </div>
+            <!-- contents -->
+            <div class="fixed top-12 bottom-12 left-0 right-0 p-4">
+                <!-- video player div -->
+                <div id="@{_videoPlayerDiv}" class=""></div>
 
-        <!-- Contents -->
-        <div class="fixed bottom-14 left-0 right-0 top-0 bg-slate-800">
-            <!-- video section -->
-            <div id="@{_videoSection}" class="hidden">
-                <!-- title -->
-                <div class="w-full p-2">
-                    <h1 class="text-slate-400 font-sans text-lg text-center" >@{title}</h1>
-                </div>
+                <!-- child controls -->
+                <!--div id="@{_controlsDiv}" class=""></div-->
+            </div>
 
-                <!-- video div -->
-                <div class="aspect-video mr-2 ml-2 bg-black rounded-lg">
+            <!-- menu -->
+            <div class="fixed bottom-0 left-0 right-0 h-14 bg-slate-900">
 
-                </div>
-
-                <!-- description -->
-                <div class="w-full p-2">
-                    <p class="text-slate-400 font-sans text-sm" >@{description}</p>
-                </div>
             </div>
         </div>
         `;
     }
 
     Init() {
-
-    }
-
-    ShowVideo() {
-        this._videoSection.style.display = "block";
-    }
-
-    HideVideo() {
-        this._videoSection.style.display = "none";
+        // // Add video player
+        this.SetData({
+            videoPlayer: {
+                controlType: "videoPlayer",
+                parentElement: "_videoPlayerDiv",
+                hlsUrl: "https://za-scc-stream.bccsa.org/toplevelmanifest.m3u8",
+                imageUrl: "img/live-poster.jpg"
+            }
+        })
     }
 }
---
+++ client/controls/videoPlayer.js
@@ -0,0 +1,49 @@
+class videoPlayer extends ui {
+    constructor() {
+        super();
+        this.imageUrl = "";
+        this.hlsUrl = "";
+        this._player = undefined;   // Used for videoJS player object reference
+    }
+
+    get html() {
+        return `
+        <!-- video div -->
+        <div class="aspect-video max-w-[600pt] rounded-lg m-auto mb-4 overflow-hidden">
+            <video
+                id="@{_playerElement}"
+                class="video-js w-full h-full rounded-lg"
+                controls
+                preload="auto"
+                data-setup="{}"
+            >
+                <p class="vjs-no-js">
+                    To view this video please enable JavaScript, and consider upgrading to a
+                    web browser that
+                    <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+                </p>
+            </video>
+        </div>`;
+    }
+
+    Init() {
+        // Initialize player
+        this._player = videojs(this._playerElement.id);
+        if (this.hlsUrl) {
+            this._player.src(this.hlsUrl);
+        }
+        if (this.imageUrl) {
+            this._player.poster(this.imageUrl);
+        }
+
+        // Update player source on hlsUrl change
+        this.on('hlsUrl', url => {
+            this._player.source(url);
+        });
+
+        // Update player posert on imageUrl change
+        this.on('imageUrl', url => {
+            this._player.poster(url);
+        });
+    }
+}
