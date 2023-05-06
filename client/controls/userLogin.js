458d23657492fd561bb58dde94db90ca3462690f
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2023-05-06 09:59:28 +0200

    Add user info

--- client/controls/userLogin.js
+++ client/controls/userLogin.js
@@ -3,13 +3,17 @@ class userLogin extends ui {
         super();
         this.cssClass = "w-full"
         this.userName = '';
+        this.userPicture = '';
         this.isAuthenticated = false; // Set from index.js Auth0 logic
     }
 
     get html() {
         return `
         <div class="flex flex-col items-center w-full">
-            <span id="@{_userName}" style="display: none;">@{userName}</span>
+            <div id="@{_userDetails}" style="display: none;" class="">
+                <img src="@{userPicture}" class="m-auto mb-2 rounded-full w-20"></img>
+                <p class="text-slate-100">Welcome <span>@{userName}</span>!</p>
+            </div>
             <button id="@{_login}" type="button" class="m-4 rounded bg-slate-400 hover:bg-slate-300 h-12 w-48 max-w-full text-slate-800 font-bold py-2 px-4">
                 <div class="w-max inline-flex items-center align-middle">
                     <div class="icon-[material-symbols--login-rounded] h-6 w-6 mr-2"></div>
@@ -29,8 +33,21 @@ class userLogin extends ui {
     Init() {
         this.showLogin(this._topLevelParent.appFrame.isAuthenticated);
 
-        this._topLevelParent.appFrame.on('isAuthenticated', val => {
-            this.showLogin(val);
+        this._topLevelParent.appFrame.on('isAuthenticated', auth => {
+            this.showLogin(auth);
+
+            if (auth) {
+                // Get the username
+                auth0Client.getUser().then(data => {
+                    if (data && data.name) {
+                        this.userName = data.name;
+                        this.userPicture = data.picture;
+                    }
+                });
+            } else {
+                this.userName = "";
+                this.userPicture = "";
+            }
         });        
 
         this._login.addEventListener('click', e => {
@@ -59,12 +76,14 @@ class userLogin extends ui {
     showLogin(login) {
         if (login) {
             this._login.style.display = "none";
-            this._userName.style.display = "block";
+            this._userDetails.style.display = "block";
             this._logout.style.display = "block";
         } else {
             this._logout.style.display = "none";
             this._login.style.display = "block";
-            this._userName.style.display = "none";
+            this._userDetails.style.display = "none";
         }
     }
 }
+
+//<div id="@{_userDetails}" style="display: none;" class="grid justify-items-center">
