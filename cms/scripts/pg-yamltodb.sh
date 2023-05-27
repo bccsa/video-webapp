b4451b3fe2bbc20de721113d1da81871ea27ebaf
Oswald Slabbert <77624298+oslabbie@users.noreply.github.com>
2023-05-27 11:50:21 +0200

    hotfix/expect send

--- cms/scripts/pg-yamltodb.sh
+++ cms/scripts/pg-yamltodb.sh
@@ -1,5 +1,5 @@
 # Varaibrles 
-#!/usr/bin/expect -f
+#!/usr/bin/expect
 HOST=$1
 PORT=$2
 USER=$3
@@ -11,7 +11,8 @@ OUTPUTFILE=$7
 # Export diff from dest DB 
 expect <<END
 spawn yamltodb --host $HOST --port $PORT --username $USER --password $DB $INPUTFILE -o $OUTPUTFILE
-expect "Password: " 
-send "$PASSWORD\r" 
+expect "Password: "
+send {'$PASSWORD'}
+send \r
 expect eof
 END
