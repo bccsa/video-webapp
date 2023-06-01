888bdee4288818c57a94b5a1bd6f1de45282ac01
Oswald Slabbert <77624298+oslabbie@users.noreply.github.com>
2023-06-01 14:50:33 +0200

    Docs & DB CI/CD

--- cms/scripts/pg-update.sh
+++ cms/scripts/pg-update.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
+# read env file 
+set -a; source ../../.env; set +a;
 
 # Create snapshot from DB
-bash "$SCRIPT_PATH" "$DB_HOST" "$DB_PORT" "$DB_USER" "$DB_PASSWORD" "$DB_DATABASE" "$SNAPSHOT_PATH" "$DIFF_PATH"
+bash ./pg-yamltodb.sh
 
-# Upload diff to server 
-psql "host=$DB_HOST port=$DB_PORT user=$DB_USER password=$DB_PASSWORD dbname=$DB_DATABASE" < "$DIFF_PATH"
+# apply diff to server
+psql "host=$DB_HOST port=$DB_PORT user=$DB_USER password=$DB_PASSWORD dbname=$DB_DATABASE" < "../snapshot/CMS-DIFF.sql"
