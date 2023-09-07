7aaa69633df948d0f8fbc4092dccc4cf02015789
Laurens Groeneveld <me@laurens.io>
2023-09-07 09:39:05 +0200

    Update paths in other script as well

--- cms/scripts/pg-dbtoyaml.sh
+++ cms/scripts/pg-dbtoyaml.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 # read env file 
-set -a; source ../../.env; set +a;
+set -a; source ../.env; set +a;
 OUTPUTFILE="../snapshot/CMS-DB.yaml"
 
 # Create a snapshot of your dev DB
-#dbtoyaml --host $DB_HOST --port $DB_PORT --username $DB_USER --password $DB_DATABASE > $OUTPUTFILE
+# https://pyrseas.readthedocs.io/en/latest/dbtoyaml.html
 expect <<END
 spawn dbtoyaml --host $DB_HOST --port $DB_PORT --username $DB_USER --password --no-owner --no-privileges $DB_DATABASE -o $OUTPUTFILE
 expect "Password: "
