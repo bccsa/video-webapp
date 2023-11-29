15efd6f7bf011320ecda83c18c0e7976ad69292b
Laurens Groeneveld <me@laurens.io>
2023-11-29 15:32:52 +0200

    Refactor to use configuration sheet
    Dynamically fetch all events from a separate sheet

--- server/tickets/tickets.js
+++ server/tickets/tickets.js
@@ -2,39 +2,72 @@ const google = require("./google");
 
 class tickets {
     /**
+     * Transform an Excel column name into a number
+     * @param {string} column Column name (like 'A', 'B', 'AB')
+     * @returns number
+     * From https://quickref.me/convert-the-name-of-an-excel-column-to-number
+     */
+    getColumnNumber(column) {
+        return column.split('').reduce((prev, next) => prev * 26 + parseInt(next, 36) - 9, 0) - 1
+    };
+
+    /**
      * @param {string} personId - The Brunstad SSO person/family id
      */
     getTickets(personId) {
-        return google.get("Summer Conference 2023!A1:AI1").then(headerRows => {
-            const headers = headerRows[0];
+        return google.get("Configuration!A3:H").then(conferencesArray => {
+            const conferences = conferencesArray.map(conference => {
+                return {
+                    name: conference[0],
+                    startDate: conference[1],
+                    endDate: conference[2],
+                    ticketInfoColumns: conference[3],
+                    nameColumn: conference[4],
+                    personIdColumn: conference[5],
+                    familyIdColumn: conference[6],
+                    ageColumn: conference[7],
+                }
+            });
 
-            return google.get("Summer Conference 2023!A2:AI").then(sheetData => {
-                let event = {
-                    controlType: "event",
-                    displayName: "Summer Conference 2023", // TODO dynamically set event
-                };
+            const conferencePromises = [];
 
-                // TODO take family into account
-                let tickets = sheetData.filter(row => row[3].includes(personId)); // row 3 = D = person id
+            conferences.forEach(conference => {
+                conferencePromises.push(new Promise((resolve, reject) => {        
+                    return google.get(`${conference.name}!A1:ZZ`).then(sheetData => {
+                        const headers = sheetData[0];
 
-                // Convert tickets array to object
-                tickets = tickets.map(ticket => {
-                    const obj = {
-                        controlType: "ticket",
-                    };
-                    headers.forEach((header, index) => {
-                        obj[header] = ticket[index];
-                    });
-                    return obj;
-                });
+                        let event = {
+                            controlType: "event",
+                            displayName: conference.name,
+                        };
+    
+                        // TODO take family into account
+                        const personIdColumn = this.getColumnNumber(conference.personIdColumn);
+                        let tickets = sheetData.filter(row => row[personIdColumn].includes(personId));
 
-                Object.assign(event, tickets);
-                
-                // Return a ModularUI section for tickets
+                        // Convert tickets array to modularUI object, with the headers as keys
+                        tickets = tickets.map(ticket => {
+                            const obj = {
+                                controlType: "ticket",
+                            };
+                            headers.forEach((header, index) => {
+                                obj[header] = ticket[index];
+                            });
+                            return obj;
+                        });
+    
+                        Object.assign(event, tickets);
+
+                        resolve(event);
+                    }).catch(err => reject(err));
+                }))
+            });
+
+            return Promise.all(conferencePromises).then(events => {
                 return {
                     controlType: "section",
                     displayName: "Tickets",
-                    '0': event, // TODO dynamically set event
+                    ...events,
                 };
             });
         });
