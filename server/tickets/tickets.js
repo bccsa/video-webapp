ddabbf3dace9ae59e9cb73dc9f4b9e33c0558442
Laurens Groeneveld <me@laurens.io>
2023-12-07 12:15:29 +0200

    Move colors to ticket info
    + filter ticket info on serverside

--- server/tickets/tickets.js
+++ server/tickets/tickets.js
@@ -128,11 +128,13 @@ class tickets {
             };
 
             conference.ticketInfoColumns.forEach((column) => {
-                ticketObject[column] = {
-                    controlType: "ticketInfo",
-                    title: headers[column],
-                    content: ticket[column],
-                };
+                if (ticket[column] && ticket[column] !== "0" && ticket[column].toLowerCase() !== "no") {
+                    ticketObject[column] = {
+                        controlType: "ticketInfo",
+                        title: headers[column],
+                        content: ticket[column],
+                    };
+                }
             });
 
             return ticketObject;
